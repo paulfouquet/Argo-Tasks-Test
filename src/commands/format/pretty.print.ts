613c637b7a2fcb4b64cae20a3abfdca0c7390412
Paul Fouquet <pfouquet@linz.govt.nz>
2023-09-11 11:33:21 +1200

    fix: formatting

--- src/commands/format/pretty.print.ts
+++ src/commands/format/pretty.print.ts
@@ -3,7 +3,7 @@ import { command, positional, string, optional, option } from 'cmd-ts';
 import { getFiles } from '../../utils/chunk.js';
 import { config, registerCli, verbose } from '../common.js';
 import { CliInfo } from '../../cli.info.js';
-import * as prettier from "prettier"
+import * as prettier from 'prettier';
 import { logger } from '../../log.js';
 import { basename } from 'path';
 
@@ -38,23 +38,23 @@ export const commandPrettier = command({
 
   async handler(args) {
     registerCli(this, args);
-    
     const files = await getFiles([args.path]);
     // This should be already filtered by `list` returning only json files
     const jsonFiles = files.flat().filter(isJson);
     if (jsonFiles.length === 0) throw new Error('No Files found');
     // test if can access on of the file
     if (jsonFiles[0]) await fsa.head(jsonFiles[0]);
-    await Promise.all(
-      jsonFiles.map((f: string) => formatFile(f, args.target)),
-    );
+    await Promise.all(jsonFiles.map((f: string) => formatFile(f, args.target)));
   },
 });
 
-async function formatFile(path: string, target: string = ""): Promise<void> {
+async function formatFile(path: string, target = ''): Promise<void> {
   logger.info({ file: path }, 'Prettier:Format');
-  const formatted = await prettier.format(JSON.stringify(await fsa.readJson(path)), { ...defaultPrettierFormat, parser: 'json' });
-  if (target){
+  const formatted = await prettier.format(JSON.stringify(await fsa.readJson(path)), {
+    ...defaultPrettierFormat,
+    parser: 'json',
+  });
+  if (target) {
     // FIXME: can be duplicate files
     path = fsa.join(target, basename(path));
   }
